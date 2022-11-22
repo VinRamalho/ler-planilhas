0ffc0023b5b1e579547606f029ba1bd1a8f633e4
vinramalho <vinramalho10@gmail.com>
2022-11-22 12:32:06 -0300

    adicinando botao de scroll pra cima

--- front/script.js
+++ front/script.js
@@ -248,6 +248,27 @@ const apagaTabela = async () => {
     }
 }
 
+
+$(function () {
+    $(document).on('scroll', function () {
+        if ($(window).scrollTop() > 100) {
+            $('.smoothscroll-top').addClass('show');
+        } else {
+            $('.smoothscroll-top').removeClass('show');
+        }
+    });
+    $('.smoothscroll-top').on('click', scrollToTop);
+});
+
+function scrollToTop() {
+    verticalOffset = typeof (verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $('body');
+    offset = element.offset();
+    offsetTop = offset.top;
+    $('html, body').animate({ scrollTop: 50 }, 100).animate({ scrollTop: 0 }, 50);
+}
+
+
 // ESSE EVENTO NO INPUT, ASSIM UQE SUBIR UM ARQUIVO CHAMA A FUNCAO selecaoDeArquivo.
 document.getElementById('upload').addEventListener('change', selecaoDeArquivo);
 
